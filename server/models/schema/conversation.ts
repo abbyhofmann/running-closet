aeaab8b2d5c579782e11ddd802326701a3f88893
kstuntz <stuntz.k@northeastern.edu>
2024-10-24 14:47:33 -0400

    Lint errors'

--- server/models/schema/conversation.ts
+++ server/models/schema/conversation.ts
@@ -2,16 +2,16 @@ import { Schema } from 'mongoose';
 
 /**
  * Mongoose schema for the Conversation collection.
- * 
+ *
  * This schema defines the structure of conversation used to define conversations between different users.
  * Each conversation includes the following fields:
  * - `users`: The users who are participating in the conversation.
  */
 const conversationSchema: Schema = new Schema(
-    {  
-        users: [ { type: Schema.Types.ObjectId, ref: 'User' } ],
-    },
-    { collection: 'Conversation' }
+  {
+    users: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  },
+  { collection: 'Conversation' },
 );
 
 export default conversationSchema;
--- server/models/schema/message.ts
+++ server/models/schema/message.ts
@@ -2,7 +2,7 @@ import { Schema } from 'mongoose';
 
 /**
  * Mongoose schema for the Message collection.
- * 
+ *
  * This schema defines the structure for storing messages in the database. Messages are a part of Conversations.
  * Each message includes the following fields:
  * - `messageContent`: The content of the message.
@@ -12,18 +12,18 @@ import { Schema } from 'mongoose';
  * - `readBy`: An array of users that have read the message.
  */
 const messageSchema: Schema = new Schema(
-    {
-        messageContent: {
-            type: String,
-        },
-        conversation: { type: Schema.Types.ObjectId, ref: 'Conversation' },
-        sender: { type: Schema.Types.ObjectId, ref: 'User' },
-        sentAt: {
-            type: Date,
-        },
-        readBy: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  {
+    messageContent: {
+      type: String,
     },
-    { collection: 'Message' }
+    conversation: { type: Schema.Types.ObjectId, ref: 'Conversation' },
+    sender: { type: Schema.Types.ObjectId, ref: 'User' },
+    sentAt: {
+      type: Date,
+    },
+    readBy: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  },
+  { collection: 'Message' },
 );
 
 export default messageSchema;
