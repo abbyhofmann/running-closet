daa752045802e1021b8545fc408d4a385b7dc7fd
elizabeth-jamison <102839858+elizabeth-jamison@users.noreply.github.com>
2024-11-12 22:49:11 -0500

    Fixing console error from notification dropdown (#38)

--- client/src/hooks/useNotificationDropdown.ts
+++ client/src/hooks/useNotificationDropdown.ts
@@ -10,41 +10,6 @@ const useNotificationDropdown = () => {
   const [notifications, setNotifications] = useState<Notification[]>([]);
 
   const fetchNotifications = () => {
-    // add in call to backend when implemented.
-    // example notifications
-    // *** ONCE NOTIFICATION SERVICE IS IN PLACE, REPLACE WITH THE NOTIFICATIONS FOR THE CURRENT USER ***
-    // const user1: User = { username: 'user1' };
-    // const user2: User = { username: 'user2' };
-    // const user3: User = { username: 'user3' };
-    // const conversation1: Conversation = { users: [user1, user2] };
-    // const conversation2: Conversation = { users: [user1, user3] };
-    // const conversation3: Conversation = { users: [user1, user2, user3] };
-    // const message1: Message = {
-    //   messageContent: 'example message 1',
-    //   conversation: conversation1,
-    //   sender: user2,
-    //   sentAt: new Date(),
-    //   readBy: [user1],
-    // };
-    // const message2: Message = {
-    //   messageContent: 'example message 2',
-    //   conversation: conversation2,
-    //   sender: user3,
-    //   sentAt: new Date(),
-    //   readBy: [user1],
-    // };
-    // const message3: Message = {
-    //   messageContent: 'example message 3',
-    //   conversation: conversation3,
-    //   sender: user3,
-    //   sentAt: new Date(),
-    //   readBy: [user1, user2],
-    // };
-    // const notificationList: Notification[] = [
-    //   { user: 'user1', message: message1 },
-    //   { user: 'user1', message: message2 },
-    //   { user: 'user1', message: message3 },
-    // ];
     const notificationList: Notification[] = [];
     return notificationList;
   };
@@ -52,7 +17,7 @@ const useNotificationDropdown = () => {
   useEffect(() => {
     const notifList: Notification[] = fetchNotifications();
     setNotifications(notifList);
-  }, [notifications]);
+  }, []);
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setDropdown(event.currentTarget);
